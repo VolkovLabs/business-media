bf316ec7809de417b419c3814f89249f642b4e7c
vitPinchuk <vit.pinchuck@gmail.com>
2024-04-12 15:35:29 +0300

    first step added  @playwright/test and @grafana/plugin-e2e removed     "@grafana/e2e": "^10.3.3", and  "@grafana/e2e-selectors":

---
+++ test/panel.spec.ts
@@ -0,0 +1,8 @@
+import { PanelEditPage, expect, test } from '@grafana/plugin-e2e';
+import { TEST_IDS } from '../src/constants';
+
+test.describe('ImagePanel', () => {
+  /**
+   * TO DO test cases
+   */
+});
