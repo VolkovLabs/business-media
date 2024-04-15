ed89735fea9e8a32d29eda766512bd91d52fc97c
vitPinchuk <vit.pinchuck@gmail.com>
2024-04-15 16:04:56 +0300

    updated: added first test

--- test/panel.spec.ts
+++ test/panel.spec.ts
@@ -1,8 +1,23 @@
-import { PanelEditPage, expect, test } from '@grafana/plugin-e2e';
-import { TEST_IDS } from '../src/constants';
+import { test, expect } from '@grafana/plugin-e2e';
 
-test.describe('ImagePanel', () => {
-  /**
-   * TO DO test cases
-   */
+test.describe('panel-datalinks panel', () => {
+  test('should display message in case panel data is empty', async ({
+    gotoPanelEditPage,
+    readProvisionedDashboard,
+  }) => {
+    const dashboard = await readProvisionedDashboard({ fileName: 'e2e.json' });
+    const panelEditPage = await gotoPanelEditPage({ dashboard, id: '2' });
+    await expect(panelEditPage.panel.locator.getByTestId('data-testid Alert warning')).toContainText(
+      'Nothing to display...'
+    );
+  });
+
+  test('should display image', async ({ gotoPanelEditPage, readProvisionedDashboard }) => {
+    const dashboard = await readProvisionedDashboard({ fileName: 'e2e.json' });
+    const panelEditPage = await gotoPanelEditPage({ dashboard, id: '1' });
+
+    const element = await panelEditPage.panel.locator.getByTestId('data-testid panel');
+
+    // TO DO
+  });
 });
