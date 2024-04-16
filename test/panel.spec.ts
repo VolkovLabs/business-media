502a3780f172506f9c5affbd98971fa3d64b820c
Vitaly Pinchuk <146737590+vitPinchuk@users.noreply.github.com>
2024-04-16 16:28:07 +0300

    Add plugin e2e tests and remove cypress (#101)
    * first step added  @playwright/test and @grafana/plugin-e2e removed     "@grafana/e2e": "^10.3.3", and  "@grafana/e2e-selectors":
    
    * upd gitignore
    
    * lock file updated
    
    * updated: added first test
    
    * e2e action
    
    * updated add e2e screenshot
    
    * check e2e action GH
    
    * revert e2e file
    
    * Update actions
    
    * updated config file; removed unnecessary lines
    
    * updated typo
    
    * coker file and congif file were upd.
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: Mikhail Volkov <mikhail@volkovlabs.io>

---
+++ test/panel.spec.ts
@@ -0,0 +1,52 @@
+import { test, expect } from '@grafana/plugin-e2e';
+
+test.describe('Base64 Image/PDF panel', () => {
+  test('should display message in case panel data is empty', async ({
+    gotoPanelEditPage,
+    readProvisionedDashboard,
+  }) => {
+    /**
+     * Use e2e.json dashboard
+     */
+    const dashboard = await readProvisionedDashboard({ fileName: 'e2e.json' });
+
+    /**
+     * Go to panel Edit page
+     */
+    const panelEditPage = await gotoPanelEditPage({ dashboard, id: '2' });
+
+    /**
+     * Alert text should be
+     */
+    await expect(panelEditPage.panel.locator.getByTestId('data-testid Alert warning')).toContainText(
+      'Nothing to display...'
+    );
+  });
+
+  test('should display image', async ({ page }) => {
+    /**
+     * Load page and open menu
+     */
+    await page.getByTestId('data-testid Toggle menu').click();
+
+    /**
+     * Go to Dashboards
+     */
+    await page.getByRole('link', { name: 'Dashboards' }).click();
+
+    /**
+     * Go to E2E dashboard
+     */
+    await page.getByRole('link', { name: 'E2E' }).click();
+
+    /**
+     * Check screenshot
+     */
+    await expect(page).toHaveScreenshot('actual-screenshot.png');
+
+    /**
+     * Compare screenshot actual
+     */
+    await expect(await page.screenshot()).toMatchSnapshot('actual-screenshot.png', { threshold: 0.3 });
+  });
+});
