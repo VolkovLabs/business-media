deb4c16b1e35dbaa73be672d1ba0b17b8a464c15
Mikhail Volkov <mikhail@volkovlabs.io>
2024-06-20 15:42:10 -0400

    Fix downloading

--- src/components/ImagePanel/ImagePanel.tsx
+++ src/components/ImagePanel/ImagePanel.tsx
@@ -404,7 +404,7 @@ export const ImagePanel: React.FC<Props> = ({ options, data, width, height, repl
                 <ToolbarButton
                   icon="save"
                   onClick={() => {
-                    saveAs(media);
+                    saveAs(imageUrl || media);
                   }}
                   data-testid={TEST_IDS.panel.buttonDownload}
                 >
